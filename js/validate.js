2876c69ec34186aa7e8273eb43dbe08ea12aafc0
fedeminatta <minattafederico@gmail.com>
2024-05-16 14:10:49 -0300

    validacion

--- js/validate.js
+++ js/validate.js
@@ -1,22 +1,45 @@
-function validateEmailInput() {
-    const emailInput = document.getElementById('email').value;
-    const errorEmail = document.getElementById('error-email');
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    if (emailRegex.test(emailInput)) {
-      errorEmail.textContent = ""; // Email is valid
-    } else {
-      errorEmail.textContent = "Please enter a valid email address.";
-    }
-};
+const formContacto = document.getElementById('form-contacto');
 
-function validateNumeroInput(){
-    const numeroInput = document.getElementById('telefono').value;
-    const errorNumero = document.getElementById('error-numero');
-    const numeroRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/; //este regex necesita mejorar
-    if (numeroRegex.test(numeroInput)) {
-        errorNumero.textContent = "" // numero is valid;
-    } else {
-        errorNumero.textContent = "Please enter a valid telephone number."; 
-    };
+let emailValido = false;
+let numeroValido = false;
+
+formContacto.addEventListener('submit', (event) => {
+	event.preventDefault();
+	validateEmailInput();
+	validateNumeroInput();
+
+	if (numeroValido && emailValido) {
+		datosValidos();
+	}
+});
+
+function validateEmailInput() {
+	const emailInput = document.getElementById('email').value;
+	const errorEmail = document.getElementById('error-email');
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+	if (emailRegex.test(emailInput)) {
+		errorEmail.textContent = ''; // Email es valido
+		emailValido = true;
+	} else {
+		errorEmail.textContent = 'Por favor ingresa una direction de correo válida';
+	}
+}
+
+function validateNumeroInput() {
+	const numeroInput = document.getElementById('telefono').value;
+	const errorNumero = document.getElementById('error-numero');
+	const numeroRegex =
+		/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/; //este regex necesita mejorar
+	if (numeroRegex.test(numeroInput)) {
+		errorNumero.textContent = ''; // numero es valido;
+		numeroValido = true;
+	} else {
+		errorNumero.textContent = 'Por favor ingresa un número válido.';
+	}
+}
+
+function datosValidos() {
+	const datosValidos = document.getElementById('datos-validos');
+	datosValidos.textContent = 'Los datos han sido enviados correctamente.';
 }
